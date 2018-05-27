a615de8c02512edffdac06ff619cb745a96afc79
Chester <Chester@DESKTOP-255UKLM>
2018-05-27 09:50:48 +0300

    test

--- CityTowerServer/Program.cs
+++ CityTowerServer/Program.cs
@@ -16,6 +16,7 @@ namespace CityTowerServer
             using (var unitOfWork = new UnitOfWork(new CityTowerDataBase()))
             {
                 var productByName = unitOfWork.GoodsList.GetProductByName("Phone");
+
             }
         }
     }
